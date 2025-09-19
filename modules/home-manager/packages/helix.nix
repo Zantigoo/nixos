487971163a2bb3e092974a3bbcca11b6aedb16f8
Zantigo <Zantigo@pm.me>
2025-09-19 11:06:53 -0400

    Sep 19
    Oops, already had Helix

--- modules/home-manager/packages/helix.nix
+++ modules/home-manager/packages/helix.nix
@@ -1,11 +1,17 @@
 {
+  pkgs,
+  lib,
   ...
-}: 
-
-{
-    programs.helix = {
-        enable = false;
-        defaultEditor = false;
-        
-    };
+} : {
+  
+  programs.helix = {
+    enable = true;
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+      }
+    ];
+  };
 }
